fe30c9853099438390f09cc1b9975feb7b6d57b8
eakech7 <tessakech@gmail.com>
2025-06-30 09:59:27 +0300

    erica

---
+++ 03_algorithms/searching/linear_search.py
@@ -0,0 +1,20 @@
+def linear_search(values):
+    target = int(input("Enter values to serach: "))
+    for index, item in enumerate (values):
+        print(f"Current value is {item}")
+        if item == target:
+            return index
+        
+    return -1
+    
+values = [-1,9,8,7,6,5,4,3,-2,1]
+print(f"List of values {values}")
+
+ans = linear_search(values)
+
+if ans != -1:
+    print(f"{values[ans]} found at index {ans}")
+else:
+    print("not found")
+    
+print(ans)
