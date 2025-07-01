f2f45b96b492576f0d5742c1d7afd46d8960194b
eakech7 <tessakech@gmail.com>
2025-07-01 16:06:52 +0300

    linear search

--- 03_algorithms/searching/linear_search_2.py
+++ 03_algorithms/searching/linear_search_2.py
@@ -1,13 +1,12 @@
-<<<<<<< HEAD
+# comment
 def linear_search(values):
-    target = int(input("Enter values to serach: "))
-    for index, item in enumerate (values):
-        print(f"Current value is {item}")
+    target = int(input("Enter values to search : "))
+    for index, item in enumerate(values):
         if item == target:
             return index
         
     return -1
-    
+
 values = [-1,9,8,7,6,5,4,3,-2,1]
 print(f"List of values {values}")
 
@@ -16,9 +15,4 @@ ans = linear_search(values)
 if ans != -1:
     print(f"{values[ans]} found at index {ans}")
 else:
-    print("not found")
-    
-print(ans)
-=======
- 
->>>>>>> 6a74da2c137cd10f45c06333a2b1522f11cae891
+    print("Not Found")
